5c5d641a383ffe6365830d95f00d2d1a58e8d82f
GoodLGames <88364745+FuneralPyreGames@users.noreply.github.com>
2021-09-28 23:42:35 -0400

    bfgdffd

--- Peggle Type Game/Assets/Scripts/Dialogue/DialogueManager.cs
+++ Peggle Type Game/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -5,15 +5,27 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
+    //Dialogue manager variables
     public TextMeshProUGUI nameText;
-    public TextMeshProUGUI dialogueText;
+    public TextMeshProUGUI dialogueText;    
     public TextMeshProUGUI firstButtonText;
+    public TextMeshProUGUI secondButtonText;
+    public TextMeshProUGUI thirdButtonText;
     private Queue<string> sentences;
     private Queue<string> names;
-    private Queue<int> buttons;
+    private Queue<int> buttons;    
     public GameObject firstButton;
     public GameObject secondButton;
     public GameObject thirdButton;
+    public GameObject Continue;
+    private string button1TextIf1ButtonNeeded;
+    private string button1TextIf2ButtonsNeeded;
+    private string button2TextIf2ButtonsNeeded;
+    private string button1TextIf3ButtonsNeeded;
+    private string button2TextIf3ButtonsNeeded;
+    private string button3TextIf3ButtonsNeeded;
+    public int actionOnEnd;
+    public DialogueObject nextDialogueObject;
     void Start()
     {
         sentences = new Queue<string>();
@@ -22,8 +34,17 @@ public class DialogueManager : MonoBehaviour
     }
     public void StartDialogue(DialogueObject dialogue)
     {
+        button1TextIf1ButtonNeeded = dialogue.button1TextIf1ButtonNeeded;
+        button1TextIf2ButtonsNeeded = dialogue.button1TextIf2ButtonsNeeded;
+        button2TextIf2ButtonsNeeded = dialogue.button2TextIf2ButtonsNeeded;
+        button1TextIf3ButtonsNeeded = dialogue.button1TextIf3ButtonsNeeded;
+        button2TextIf3ButtonsNeeded = dialogue.button2TextIf3ButtonsNeeded;
+        button3TextIf3ButtonsNeeded = dialogue.button3TextIf3ButtonsNeeded;
+        actionOnEnd = dialogue.actionOnEnd;
+        nextDialogueObject = dialogue.nextDialogueObject;
         sentences.Clear();
         names.Clear();
+        LeanTween.scale(gameObject, new Vector3 (1,1,1), 0.25f);
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
@@ -42,21 +63,35 @@ public class DialogueManager : MonoBehaviour
     {
         switch(numOfButtons)
         {
+            case 0:
+                Continue.SetActive(true);
+                firstButton.SetActive(false);
+                secondButton.SetActive(false);
+                thirdButton.SetActive(false);
+                break;
             case 1:
+                Continue.SetActive(false);
                 firstButton.SetActive(true);
                 secondButton.SetActive(false);
                 thirdButton.SetActive(false);
-                firstButtonText.text = "Continue";
+                firstButtonText.text = button1TextIf1ButtonNeeded;
                 break;
             case 2:
+                Continue.SetActive(false);
                 firstButton.SetActive(true);
                 secondButton.SetActive(true);
                 thirdButton.SetActive(false);
+                firstButtonText.text = button1TextIf2ButtonsNeeded;
+                secondButtonText.text = button2TextIf2ButtonsNeeded;
                 break;
             case 3:
+                Continue.SetActive(false);
                 firstButton.SetActive(true);
                 firstButton.SetActive(true);
                 firstButton.SetActive(true);
+                firstButtonText.text = button1TextIf3ButtonsNeeded;
+                secondButtonText.text = button2TextIf3ButtonsNeeded;
+                thirdButtonText.text = button3TextIf3ButtonsNeeded;
                 break;
         }
     }
@@ -86,13 +121,22 @@ public class DialogueManager : MonoBehaviour
     }
     void EndDialogue()
     {
+        if (actionOnEnd == 0){ //Completely end dialogue
+            LeanTween.scale(gameObject, new Vector3 (0,0,0), 0.25f);
+            SetDefaultDialogue();
+        }
+        if (actionOnEnd == 1){ //Load next dialogue object
+            StartDialogue(nextDialogueObject); 
+        }
+        if (actionOnEnd == 2){ //Load pachinko level
+
+        }
+        if (actionOnEnd == 3){ //Custom ending for scene 1
+
+        }
+    }
+    void SetDefaultDialogue(){
         dialogueText.text = "Default Dialogue";
         nameText.text = "Default Name";
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Peggle Type Game/Assets/Scripts/Dialogue/DialogueObject.cs
+++ Peggle Type Game/Assets/Scripts/Dialogue/DialogueObject.cs
@@ -2,21 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
+[System.Serializable]
 public class DialogueObject : ScriptableObject
 {
-    [Header("Dialogue")]
-    [Space(5)]
     public string[] names;
     [TextArea(3, 10)]
     public string[] sentences;
     public string speakerName;
-    [Header("Options")]
-    [Space(5)]
     public int[] buttonsNeeded;
+    public AudioClip[] voiceClip;
+    public bool audioOn = false;
     public string button1TextIf1ButtonNeeded;
     public string button1TextIf2ButtonsNeeded;
     public string button2TextIf2ButtonsNeeded;
     public string button1TextIf3ButtonsNeeded;
     public string button2TextIf3ButtonsNeeded;
     public string button3TextIf3ButtonsNeeded;
+    public DialogueObject nextDialogueObject;
+    public DialogueObject decision1Switch;
+    public DialogueObject decision2switch;
+    public DialogueObject decision3switch;
+    public string sceneToSwitchTo;
+    public int actionOnEnd;
+    // 0 = End Dialogue Normally
+    // 1 = Load Next Dialogue
+    // 2 = Load Pachinko Scene
 }
